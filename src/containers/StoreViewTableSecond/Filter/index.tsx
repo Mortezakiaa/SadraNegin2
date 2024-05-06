af2d98127b5ee286bdbfe6d3cee95cf34d371930
morteza kia <morteza.kia8700@gmail.com>
2024-05-06 14:42:45 +0330

    add name filter

--- src/containers/StoreViewTableSecond/Filter/index.tsx
+++ src/containers/StoreViewTableSecond/Filter/index.tsx
@@ -1,70 +1,82 @@
-import React, { useState } from 'react';
-import { StoreSearch, StoreSearchFilter } from '../../../models/Store/Search';
-import { Grid, TextField, Switch, FormControlLabel } from '@material-ui/core';
-
+import React, { useState } from "react";
+import { StoreSearch, StoreSearchFilter } from "../../../models/Store/Search";
+import { Grid, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 interface FilterTableContainerProps {
-    mainState: StoreSearch;
+  mainState: StoreSearch;
 }
 
 export const FilterTableContainer = ({
-    mainState,
+  mainState,
 }: FilterTableContainerProps) => {
-    const [filter, setFilter] = useState<StoreSearchFilter>({
-        type: '',
-        color: '',
-        dahane: '',
-        name:'',
-        sortManfi: false,
-    });
+  const [filter, setFilter] = useState<StoreSearchFilter>({
+    type: "",
+    color: "",
+    dahane: "",
+    name: "",
+    sortManfi: false,
+  });
 
-    const onChangeSortModeHandler = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
-        setFilter({ ...filter, sortManfi: checked });
-        mainState.getList({ ...filter, sortManfi: checked })
-    }
+  const onChangeSortModeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => {
+    setFilter({ ...filter, sortManfi: checked });
+    mainState.getList({ ...filter, sortManfi: checked });
+  };
 
-    return (
-        <>
-            <Grid container item style={{ justifyContent: 'center', width: '100%' }} >
-                <form style={{ display: 'flex', justifyContent: 'space-around', width: '100%', marginBottom: '20px' }}>
-                    <FormControlLabel control={
-                        <Switch
-                            checked={filter.sortManfi}
-                            onChange={onChangeSortModeHandler}
-                        />
-                    } label="ترتیب منفی" />
-                    {/* <TextField
-                        id="outlined-basic"
-                        label="فیلتر نوع"
-                        variant="outlined"
-                        type="text"
-                        name="filter2"
-                        value={filter.type}
-                        onChange={e => setFilter({ ...filter, type: e.target.value })}
-                        onKeyDown={e => {
-                            if (e.key === 'Enter') {
-                                e.preventDefault();
-                                mainState.getList({ ...filter })
-                            }
-                        }} /> */}
-                    <TextField
-                        id="outlined-basic"
-                        label="فیلتر رنگ"
-                        variant="outlined"
-                        type="text"
-                        name="filter"
-                        value={filter.color}
-                        onChange={e => setFilter({ ...filter, color: e.target.value })}
-                        onKeyDown={e => {
-                            if (e.key === 'Enter') {
-                                e.preventDefault();
-                                mainState.getList({ ...filter })
-                            }
-                        }} />
-                </form>
-            </Grid>
-
-        </>
-    )
-}
-
+  return (
+    <>
+      <Grid container item style={{ justifyContent: "center", width: "100%" }}>
+        <form
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "100%",
+            marginBottom: "20px",
+          }}
+        >
+          <FormControlLabel
+            control={
+              <Switch
+                checked={filter.sortManfi}
+                onChange={onChangeSortModeHandler}
+              />
+            }
+            label="ترتیب منفی"
+          />
+          <TextField
+            id="outlined-basic"
+            label="فیلتر نام"
+            variant="outlined"
+            type="text"
+            name="filter2"
+            value={filter.name}
+            onChange={(e) => setFilter({ ...filter, name: e.target.value })}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                mainState.getList({ ...filter });
+              }
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="فیلتر رنگ"
+            variant="outlined"
+            type="text"
+            name="filter"
+            value={filter.color}
+            onChange={(e) => setFilter({ ...filter, color: e.target.value })}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                mainState.getList({ ...filter });
+              }
+            }}
+          />
+        </form>
+      </Grid>
+    </>
+  );
+};
